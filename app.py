18a916aa46212c7f4ae2ddecb4eb65473646aa46
Oli M <45511594+OliverGuy79@users.noreply.github.com>
2024-04-06 17:47:55 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+
+from flask import (
+    Flask,
+    redirect,
+    render_template,
+    request,
+    send_from_directory,
+    url_for,
+    send_file,
+)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    # return send_file("data/menu/Menu Goldis 1_page-0001.jpg")
+
+    return redirect(url_for("menu"))
+
+
+# @app.route("/menu1")
+# def menu1():
+#     print("Request for menu page 1 received")
+#     return send_file("data/menu/Menu Goldis A1-combined.pdf")
+
+
+# @app.route("/menu2")
+# def menu2():
+#     print("Request for menu page 2 received")
+
+#     return send_file("data/menu/Menu Goldis A1-combined.pdf")
+
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+
+if __name__ == "__main__":
+    app.run()
